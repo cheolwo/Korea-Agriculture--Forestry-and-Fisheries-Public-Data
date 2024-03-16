01b3e20db4df71ab9eb6585c61c35f55ed65f604
cheolwo <powerqkrcjfd@gmail.com>
2024-03-16 12:57:08 +0900

    .

--- 농림축산식품 공공데이터/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace 농림축산식품_공공데이터
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ 농림축산식품 공공데이터/HSK품목매핑목록조회Service.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HSKItemMapping
+{
+    public class HSK품목매핑목록조회Service
+    {
+        private readonly HttpClient _client;
+
+        public HSK품목매핑목록조회Service(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public async Task<string> GetHSKItemMappingAsync(string apiKey, int startIndex, int endIndex, string categoryCode, string productCode)
+        {
+            string apiUrl = "http://211.237.50.150:7080/openapi/sample/xml/Grid_20221207000000000652_1/1/5";
+            string queryData = $"?API_KEY={apiKey}&TYPE=json&START_INDEX={startIndex}&END_INDEX={endIndex}&CATEGORY_CODE={categoryCode}&STD_PRDLST_CODE={productCode}";
+            string requestUri = apiUrl + queryData;
+
+            try
+            {
+                HttpResponseMessage response = await _client.GetAsync(requestUri);
+                response.EnsureSuccessStatusCode(); // 상태 코드가 200-299인지 확인
+                return await response.Content.ReadAsStringAsync(); // 응답 본문을 문자열로 반환
+            }
+            catch (HttpRequestException e)
+            {
+                throw new ApplicationException("HSK Item Mapping 조회 중 오류 발생", e);
+            }
+        }
+    }
+}
